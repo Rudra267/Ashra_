4e25576c293fb06a41e0287112d5b5707b9ba82b
user.name:"Rudra267 <nrudra550@gmail.com>
2025-02-05 09:44:48 +0530

    05022025

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,6 +2,7 @@ const Fname = document.getElementById("Fname");
 const Lname = document.getElementById("Lname");
 const email = document.getElementById("email");
 const phone = document.getElementById("PhoneNumber");
+const subject = document.getElementById("Subject");
 const mess = document.getElementById("message");
 
 function sendEmail() {
@@ -16,7 +17,7 @@ function sendEmail() {
           });
     }
     else{
-         const bodyMessage = `Full Name: ${Fname.value} ${Lname.value} Email: ${email.value} Phone Number: ${phone.value} Message: ${mess.value}`;
+         const bodyMessage = `Subject: ${subject.value} Full Name: ${Fname.value} ${Lname.value} Email: ${email.value} Phone Number: ${phone.value} Message: ${mess.value} `;
          
     console.log(bodyMessage)
     var param = {
@@ -33,7 +34,8 @@ function sendEmail() {
             document.getElementById("Lname").value = "",
             document.getElementById("email").value = "",
             document.getElementById("PhoneNumber").value = "",
-            document.getElementById("message").value = ""
+            document.getElementById("message").value = "",
+            document.getElementById("Subject").value = "",
 
             Swal.fire({
                 position: "center",
